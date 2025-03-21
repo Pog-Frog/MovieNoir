098c946507b9bff548414ea2b5d3375fdb56e854
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-03-21 22:51:12 +0200

    Add hamburger-react for mobile menu and implement toggle functionality in Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
 import { FaPlay } from "react-icons/fa";
 import { Link, NavLink } from "react-router";
+import { Spin as Hamburger } from "hamburger-react";
+import { useState } from "react";
+
 
 const Header = () => {
 
@@ -18,28 +21,50 @@ const Header = () => {
         }
     ]
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
-        // TODO: Decrease the padding of the header
-        <div className="fixed z-50 h-fit flex flex-row justify-between items-center w-full px-24 py-12 outline-none border-none">
-            <Link to="/">
-                <div className="flex flex-row gap-3 items-center text-white text-4xl font-medium">
-                    <FaPlay className="text-indigo-500 scale-105" />
-                    <p>
-                        Movie Noir
-                    </p>
+        <div className="fixed top-0 z-5 w-full px-6 sm:px-10 md:px-16 lg:px-28 py-6 bg-black ">
+            <div className="w-full h-full m-auto flex flex-row items-center justify-between">
+                <Link to="/" className="shrink-0 relative z-[100] h-auto w-auto flex flex-row items-center">
+                    <div className="flex flex-row gap-3 items-center text-white text-4xl font-medium">
+                        <FaPlay className="text-indigo-500 scale-105" />
+                        <span>
+                            Movie Noir
+                        </span>
+                    </div>
+                </Link>
+                <div
+                    className={`absolute inset-0 h-screen px-6 sm:px-10 md:px-16 pt-20 bg-[#040018] flex flex-col gap-6 transition duration-500 ${menuOpen ? "translate-x-0" : "translate-x-full"
+                        } lg:relative lg:h-fit lg:p-0 lg:bg-transparent w-full lg:flex lg:flex-row lg:items-center lg:gap-0 lg:translate-x-0`}
+                >
+                    <nav className="lg:ml-auto lg:-translate-x-10">
+                        <ul className="flex lg:flex-row flex-col gap-6 mt-8 lg:mt-0 text-white text-2xl font-semibold">
+                            {headerLinks.map((link, index) => {
+                                return (
+                                    <li key={index}>
+                                        <NavLink to={link.link} className={({ isActive }) => `${isActive ? "border-b-3 border-indigo-500" : "hover:border-b-3 border-indigo-500"}`}>
+                                            {link.text}
+                                        </NavLink>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </nav>
                 </div>
-            </Link>
-            <ul className="flex flex-row gap-6 text-white text-2xl font-semibold">
-                {headerLinks.map((link, index) => {
-                    return (
-                        <li key={index}>
-                            <NavLink to={link.link} className={({ isActive }) => `${isActive ? "border-b-3 border-indigo-500" : "hover:border-b-3 border-indigo-500"}`}>
-                                {link.text}
-                            </NavLink>
-                        </li>
-                    );
-                })}
-            </ul>
+                <div className="block lg:hidden">
+                    <Hamburger
+                        toggled={menuOpen}
+                        toggle={toggleMenu}
+                        size={22}
+                        color="white"
+                        label="Menu Icon" />
+                </div>
+            </div>
         </div>
     );
 }
