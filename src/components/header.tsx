8356736d6b5d2227298c6b544356413fbe66e1a5
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-03-27 03:01:13 +0200

    Add fade animation for the header based on scrolling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,14 +22,14 @@ const Header = () => {
     ]
 
     const [menuOpen, setMenuOpen] = useState(false);
-    const [shrinkHeader, setShrinkHeader] = useState(false);
+    const [shrinkHeader, setShrinkHeader] = useState("");
 
     useEffect(() => {
         const handleScroll = () => {
             if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-                setShrinkHeader(true);
+                setShrinkHeader("md:!py-4 bg-opacity-100");
             } else {
-                setShrinkHeader(false);
+                setShrinkHeader("");
             }
         }
 
@@ -45,7 +45,7 @@ const Header = () => {
     }
 
     return (
-        <div className={`sticky top-0 left-0 right-0 z-50 w-full px-4 py-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 2xl:px-20 ${ shrinkHeader ? "bg-black" : "bg-transparent"}`}>
+        <div className={`sticky top-0 left-0 right-0 z-50 w-full px-4 py-4 md:py-6 sm:px-6 md:px-8 lg:px-12 xl:px-16 2xl:px-20 bg-opacity-0 transition-all duration-300 ${shrinkHeader}`}>
             <div className="mx-auto flex h-full max-w-screen-2xl items-center justify-between">
                 {/* Logo */}
                 <Link
