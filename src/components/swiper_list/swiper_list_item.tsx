9e17f4c8137eeb4e0bde4521a6e1ecd313e6daf4
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-04-06 23:42:36 +0200

    Fix image source fallback in SwiperListItem component

--- src/components/swiper_list/swiper_list_item.tsx
+++ src/components/swiper_list/swiper_list_item.tsx
@@ -19,7 +19,7 @@ const SwiperListItem: React.FC<SwiperListItemsProps> = ({ item, category }) => {
     return (
         <div className="w-44 flex flex-col gap-3">
             <div className="relative cursor-pointer group" onClick={() => navigate(link)}>
-                <img src={item.poster_path || item.poster_path ? backGround : ""} alt="movie_poster" className="rounded-md aspect-[1/1.5]" />
+                <img src={item.poster_path || item.poster_path ? backGround : undefined} alt="movie_poster" className="rounded-md aspect-[1/1.5]" />
                 <div className="absolute bottom-0 z-40 text-white flex flex-col gap-2 px-4 py-3 opacity-0 scale-75 transition duration-300 delay-100 group-hover:opacity-100 group-hover:scale-100">
                     <ItemRating rating={item.vote_average} small />
                 </div>
