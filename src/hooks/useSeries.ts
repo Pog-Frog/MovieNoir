4db74056d2ccc09ac29bd7734c243e524120482e
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2025-04-07 00:53:11 +0200

    Add useSWRInfinite hook for infinite scrolling in series data fetching

--- src/hooks/useSeries.ts
+++ src/hooks/useSeries.ts
@@ -2,6 +2,7 @@ import useSWR from 'swr';
 import axios from 'axios';
 import config from '../api/config';
 import { SeriesResponse } from '../interfaces/series_response.interface';
+import useSWRInfinite from 'swr/infinite';
 
 export type SeriesOption = "popular" | "top_rated" | "on_the_air" | "airing_today" | "trending" | "similar";
 
@@ -17,4 +18,26 @@ const useSeries = (option: SeriesOption = "popular", page: number = 1) => {
     }
 }
 
+const useSeriesInfiniteScroll = (option: SeriesOption = "popular", query?: string) => {
+    const fetcher = async (url: string) => axios.get(url).then(res => res.data);
+    const { data, error, isLoading, mutate, size, setSize } = useSWRInfinite<SeriesResponse>(index =>
+        query
+            ? `${config.baseUrl}search/tv?page=${index + 1}&api_key=${config.apiKey}&query=${query}`
+            : option === "trending"
+            ? `${config.baseUrl}${option}/tv/day?page=${index + 1}&api_key=${config.apiKey}`
+            : `${config.baseUrl}tv/${option}?page=${index + 1}&api_key=${config.apiKey}`,
+        fetcher
+    );
+
+    return {
+        data,
+        error,
+        isLoading,
+        mutate,
+        size,
+        setSize
+    }
+}
+
 export default useSeries;
+export { useSeriesInfiniteScroll };
